b95c7dc300433c36ae3d94cfdb2f12274f560fad
EmrullahKutlar <emrullah.ktlr@gmail.com>
2022-07-22 00:00:48 +0300

    toast hideprogressbar value changed

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ const options = {
   draggable: true,
   draggablePercent: 0.6,
   showCloseButtonOnHover: false,
-  hideProgressBar: true,
+  hideProgressBar: false,
   closeButton: "button",
   icon: true,
   rtl: false
